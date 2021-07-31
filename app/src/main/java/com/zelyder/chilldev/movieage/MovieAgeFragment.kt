909444f8c261ae577f3f30fbc86f0eb5dac7347f
Indira Temkesheva <indiratemk@yandex-team.ru>
2021-07-31 17:12:25 +0300

    add binding to moview age fragment

--- app/src/main/java/com/zelyder/chilldev/movieage/MovieAgeFragment.kt
+++ app/src/main/java/com/zelyder/chilldev/movieage/MovieAgeFragment.kt
@@ -5,15 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.zelyder.chilldev.R
+import com.zelyder.chilldev.databinding.FragmentMovieAgeBinding
 
 class MovieAgeFragment : Fragment() {
+
+    private var _binding: FragmentMovieAgeBinding? = null
+    private val binding
+        get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_movie_age, container, false)
+    ): View {
+        _binding = FragmentMovieAgeBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.rgAge.setOnCheckedChangeListener { _, checkedId ->
+        }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 }
