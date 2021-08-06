dcbfad26406217364af791d5bcc4bcc3d46b4b96
Indira Temkesheva <indiratemk@yandex-team.ru>
2021-08-06 21:53:29 +0300

    replace radio buttons with age rating layout

--- app/src/main/java/com/zelyder/chilldev/ui/movieage/MovieAgeFragment.kt
+++ app/src/main/java/com/zelyder/chilldev/ui/movieage/MovieAgeFragment.kt
@@ -1,6 +1,7 @@
 package com.zelyder.chilldev.ui.movieage
 
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,14 +20,32 @@ class MovieAgeFragment : FragmentPage<MovieAgePageBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.rgAge.setOnCheckedChangeListener { _, _ ->
-            page.swipeToNext()
+        binding.layoutAgeRating.setOnKeyListener { v, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_DOWN) {
+                when (keyCode) {
+                    KeyEvent.KEYCODE_DPAD_RIGHT -> {
+                        (v as AgeRatingLayout).moveToNext()
+                        true
+                    }
+                    KeyEvent.KEYCODE_DPAD_LEFT -> {
+                        (v as AgeRatingLayout).moveToPrevious()
+                        true
+                    }
+                    KeyEvent.KEYCODE_DPAD_CENTER -> {
+                        page.swipeToNext()
+                        true
+                    }
+                    else -> false
+                }
+            } else {
+                false
+            }
         }
     }
 
-    override fun onStop() {
-        binding.root.clearFocus()
-        super.onStop()
+    override fun onResume() {
+        super.onResume()
+        binding.layoutAgeRating.requestFocus()
     }
 
     companion object {
