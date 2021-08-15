35f398f45fbdaa69dfab1feafd2e891d8d7d44e1
Roman Zelenin <theleninroman@yandex-team.ru>
2021-08-15 19:08:52 +0300

    Refactoring di

---
+++ app/src/main/java/com/zelyder/chilldev/di/InterceptorsModule.kt
@@ -0,0 +1,62 @@
+package com.zelyder.chilldev.di
+
+import dagger.Module
+import dagger.Provides
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class NetworkInterceptor
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class LocalInterceptor
+
+@Module
+object InterceptorsModule {
+    @Provides
+    @LocalInterceptor
+    @ApplicationScope
+    fun provideLocalInterceptors(): List<Interceptor> {
+        val interceptors = mutableListOf<Interceptor>()
+        return interceptors.apply {
+            add(Interceptor { chain ->
+                val requestBuilder = chain.request().newBuilder()
+                requestBuilder.addHeader(
+                    "Authorization",
+                    "OAuth AQAAAAAn24kQAAdMKtm-VDWEMkljrl20f4nKnEk"
+                )
+                chain.proceed(requestBuilder.build())
+            })
+        }
+    }
+
+    @Provides
+    @NetworkInterceptor
+    @ApplicationScope
+    fun provideNetworkInterceptors(): List<Interceptor> {
+        val interceptors = mutableListOf<Interceptor>()
+        return interceptors.apply {
+            add(HttpLoggingInterceptor().apply { setLevel(HttpLoggingInterceptor.Level.BODY) })
+        }
+    }
+}
+
+fun OkHttpClient.Builder.addInterceptors(
+    interceptors: Iterable<Interceptor>,
+    isNetwork: Boolean
+): OkHttpClient.Builder {
+    if (!isNetwork) {
+        interceptors.forEach { interceptor ->
+            addInterceptor(interceptor)
+        }
+    } else {
+        interceptors.forEach { interceptor ->
+            addNetworkInterceptor(interceptor)
+        }
+    }
+    return this
+}
