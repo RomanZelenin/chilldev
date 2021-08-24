8dcee249f370591b0903dd290388d3d3eb5d6515
Roman Zelenin <theleninroman@yandex-team.ru>
2021-08-24 03:28:16 +0300

    Added animation for agerating

--- app/src/main/java/com/zelyder/chilldev/ui/movieage/AgeRatingLayout.kt
+++ app/src/main/java/com/zelyder/chilldev/ui/movieage/AgeRatingLayout.kt
@@ -1,6 +1,8 @@
 package com.zelyder.chilldev.ui.movieage
 
+import android.animation.ObjectAnimator
 import android.content.Context
+import android.graphics.drawable.GradientDrawable
 import android.util.AttributeSet
 import android.view.Gravity
 import android.widget.LinearLayout
@@ -20,14 +22,14 @@ class AgeRatingLayout(context: Context, attrSet: AttributeSet?) :
             field = value
             val startPosition = 0
             val endPosition = size - 1
+            for (i in selectedPosition + 1..endPosition) {
+                (this[i] as TextView).apply {
+                    configureDefaultTextView(this, i)
+                }
+            }
             for (i in startPosition..selectedPosition) {
                 (this[i] as TextView).apply {
                     configureSelectedTextView(this, i)
-                }
-            }
-            for (i in selectedPosition + 1..endPosition) {
-                (this[i] as TextView).apply {
-                    configureDefaultTextView(this)
                 }
             }
         }
@@ -36,7 +38,7 @@ class AgeRatingLayout(context: Context, attrSet: AttributeSet?) :
         removeAllViews()
         for (i in 0 until size) {
             val tvAgeRating = TextView(context).apply {
-                configureDefaultTextView(this)
+                configureDefaultTextView(this, i)
                 gravity = Gravity.CENTER
                 textSize = 12.dpToPx().toFloat()
                 text = AgeRating.values()[i].rating
@@ -60,18 +62,51 @@ class AgeRatingLayout(context: Context, attrSet: AttributeSet?) :
                 position != selectedPosition -> {
                     setBackgroundResource(R.drawable.shp_selected_age_rating_bg)
                     setTextColor(ContextCompat.getColor(context, android.R.color.black))
+
                 }
                 else -> {
-                    setBackgroundResource(R.drawable.shp_right_rounded_selected_age_rating_bg)
+                    val drawable = resources.getDrawable(
+                        R.drawable.shp_right_rounded_selected_age_rating_bg,
+                        null
+                    ) as GradientDrawable
+                    background = drawable.apply {
+                        alpha = 0
+                    }
+                    ObjectAnimator.ofFloat(this, "alpha", 0.2f, 1f).apply {
+                        addUpdateListener {
+                            drawable.alpha = (it.animatedFraction * 255).toInt()
+                        }
+                        duration = 500
+                        start()
+                    }
+                    //setBackgroundResource(R.drawable.shp_right_rounded_selected_age_rating_bg)
                     setTextColor(ContextCompat.getColor(context, android.R.color.black))
+
                 }
             }
         }
     }
 
-    private fun configureDefaultTextView(textView: TextView): TextView {
+    private fun configureDefaultTextView(textView: TextView, idx: Int): TextView {
         return textView.apply {
-            setBackgroundResource(R.drawable.shp_default_age_rating_bg)
+            if (selectedPosition == 0 && idx == selectedPosition + 1) {
+                val drawable = resources.getDrawable(
+                    R.drawable.shp_default_age_rating_bg,
+                    null
+                ) as GradientDrawable
+                background = drawable.apply {
+                    alpha = 0
+                }
+                ObjectAnimator.ofFloat(this, "alpha", 0f, 1f).apply {
+                    addUpdateListener {
+                        drawable.alpha = (it.animatedFraction * 255).toInt()
+                    }
+                    duration = 200
+                    start()
+                }
+            } else {
+                setBackgroundResource(R.drawable.shp_default_age_rating_bg)
+            }
             setTextColor(ContextCompat.getColor(context, android.R.color.white))
         }
     }
